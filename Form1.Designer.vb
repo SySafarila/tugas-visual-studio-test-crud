be6a211501abd7fda00aadcaf48e6cdf51f46ad7
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2022-01-19 07:08:46 +0700

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.tbNim = New System.Windows.Forms.TextBox()
+        Me.tbNama = New System.Windows.Forms.TextBox()
+        Me.tbAlamat = New System.Windows.Forms.TextBox()
+        Me.tbJurusan = New System.Windows.Forms.TextBox()
+        Me.btnSimpan = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        Me.btnTutup = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(27, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "NIM"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nama"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 97)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Alamat"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 136)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Jurusan"
+        '
+        'tbNim
+        '
+        Me.tbNim.Location = New System.Drawing.Point(192, 13)
+        Me.tbNim.Name = "tbNim"
+        Me.tbNim.Size = New System.Drawing.Size(100, 20)
+        Me.tbNim.TabIndex = 4
+        '
+        'tbNama
+        '
+        Me.tbNama.Location = New System.Drawing.Point(192, 55)
+        Me.tbNama.Name = "tbNama"
+        Me.tbNama.Size = New System.Drawing.Size(100, 20)
+        Me.tbNama.TabIndex = 5
+        '
+        'tbAlamat
+        '
+        Me.tbAlamat.Location = New System.Drawing.Point(192, 94)
+        Me.tbAlamat.Name = "tbAlamat"
+        Me.tbAlamat.Size = New System.Drawing.Size(100, 20)
+        Me.tbAlamat.TabIndex = 6
+        '
+        'tbJurusan
+        '
+        Me.tbJurusan.Location = New System.Drawing.Point(192, 133)
+        Me.tbJurusan.Name = "tbJurusan"
+        Me.tbJurusan.Size = New System.Drawing.Size(100, 20)
+        Me.tbJurusan.TabIndex = 7
+        '
+        'btnSimpan
+        '
+        Me.btnSimpan.Location = New System.Drawing.Point(192, 180)
+        Me.btnSimpan.Name = "btnSimpan"
+        Me.btnSimpan.Size = New System.Drawing.Size(75, 23)
+        Me.btnSimpan.TabIndex = 8
+        Me.btnSimpan.Text = "Simpan"
+        Me.btnSimpan.UseVisualStyleBackColor = True
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Location = New System.Drawing.Point(315, 180)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(75, 23)
+        Me.btnEdit.TabIndex = 9
+        Me.btnEdit.Text = "Edit"
+        Me.btnEdit.UseVisualStyleBackColor = True
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Location = New System.Drawing.Point(433, 180)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(75, 23)
+        Me.btnHapus.TabIndex = 10
+        Me.btnHapus.Text = "Hapus"
+        Me.btnHapus.UseVisualStyleBackColor = True
+        '
+        'btnTutup
+        '
+        Me.btnTutup.Location = New System.Drawing.Point(536, 180)
+        Me.btnTutup.Name = "btnTutup"
+        Me.btnTutup.Size = New System.Drawing.Size(75, 23)
+        Me.btnTutup.TabIndex = 11
+        Me.btnTutup.Text = "Tutup"
+        Me.btnTutup.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 225)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(776, 213)
+        Me.DataGridView1.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnTutup)
+        Me.Controls.Add(Me.btnHapus)
+        Me.Controls.Add(Me.btnEdit)
+        Me.Controls.Add(Me.btnSimpan)
+        Me.Controls.Add(Me.tbJurusan)
+        Me.Controls.Add(Me.tbAlamat)
+        Me.Controls.Add(Me.tbNama)
+        Me.Controls.Add(Me.tbNim)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents tbNim As TextBox
+    Friend WithEvents tbNama As TextBox
+    Friend WithEvents tbAlamat As TextBox
+    Friend WithEvents tbJurusan As TextBox
+    Friend WithEvents btnSimpan As Button
+    Friend WithEvents btnEdit As Button
+    Friend WithEvents btnHapus As Button
+    Friend WithEvents btnTutup As Button
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ Form1.vb
@@ -0,0 +1,59 @@
+﻿Imports System.Data.Odbc
+Public Class Form1
+    Dim Conn As OdbcConnection
+    Dim Cmd As OdbcCommand
+    Dim Ds As DataSet
+    Dim Da As OdbcDataAdapter
+    Dim Rd As OdbcDataReader
+    Dim MyDB As String
+    Sub Koneksi()
+        ' Memanggil database yaitu nama database kita adalah db_universitas
+        MyDB = "Driver={MySQL ODBC 3.51 Driver};Database=vb;Server=localhost;uid=root"
+        Conn = New OdbcConnection(MyDB)
+        If Conn.State = ConnectionState.Closed Then Conn.Open()
+    End Sub
+
+    Sub KondisiAwal()
+        tbNim.Text = ""
+        tbNama.Text = ""
+        tbAlamat.Text = ""
+        tbJurusan.Text = ""
+
+        tbNim.Enabled = False
+        tbNama.Enabled = False
+        tbAlamat.Enabled = False
+        tbJurusan.Enabled = False
+
+        tbNim.MaxLength = 15
+
+        btnSimpan.Text = "INPUT"
+        btnEdit.Text = "EDIT"
+        btnHapus.Text = "HAPUS"
+        btnTutup.Text = "TUUTUP"
+
+        btnSimpan.Enabled = True
+        btnEdit.Enabled = True
+        btnHapus.Enabled = True
+        btnTutup.Enabled = True
+
+        Call Koneksi()
+
+        Da = New OdbcDataAdapter("Select * From mahasiswa", Conn)
+        Ds = New DataSet
+        Da.Fill(Ds, "mahasiswa")
+        DataGridView1.DataSource = Ds.Tables("mahasiswa")
+    End Sub
+
+    Sub FieldAktif()
+        tbNim.Enabled = True
+        tbNama.Enabled = True
+        tbAlamat.Enabled = True
+        tbJurusan.Enabled = True
+
+        tbNim.Focus()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call KondisiAwal()
+    End Sub
+End Class
